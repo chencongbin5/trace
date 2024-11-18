ef5ff7cc7646d4a88c1110ff07669688b4c2e38e
chen <364649569@qq.com>
2024-11-18 22:04:00 +0800

    删除过敏信息

--- aku-trace/trace-api/src/main/java/com/akuchen/trace/api/common/enums/AppNameEnum.java
+++ aku-trace/trace-api/src/main/java/com/akuchen/trace/api/common/enums/AppNameEnum.java
@@ -11,21 +11,21 @@ import lombok.RequiredArgsConstructor;
 public enum AppNameEnum {
 
 
-    AKU_EC_DATA_REPORT( "aku-ec-data-report","aku-ec-data-report-service","https://git.silvrr.com/aku-ec-report/aku-ec-data-report.git"),
+    AKU_EC_DATA_REPORT( "aku-ec-data-report","aku-ec-data-report-service","https://xxxxxxxx.com/aku-ec-report/aku-ec-data-report.git"),
     OWNBANK_FULFILLMENT_JOB("ownbank-fulfillment-job",null,null),
     OWNBANK_FULFILLMENT_SERVICE("ownbank-fulfillment-service",null,null),
-    ORDER_VIOLATION_SERVICE("order-violation-service","order-violation-impl","https://git.silvrr.com/aku-ec-service/order-violation-service.git"),
-    ORDER_BACKGROUND_SERVICE("order-background-service","order-background-service","https://git.silvrr.com/aku-ec-service/order-service.git"),
+    ORDER_VIOLATION_SERVICE("order-violation-service","order-violation-impl","https://xxxxxxxx.com/aku-ec-service/order-violation-service.git"),
+    ORDER_BACKGROUND_SERVICE("order-background-service","order-background-service","https://xxxxxxxx.com/aku-ec-service/order-service.git"),
     VENDOR_ORDERS_BIZ("vendor-orders-biz",null,null),
-    ORDER_SERVICE_JOB("order-service-job","order-service-job","https://git.silvrr.com/aku-ec-service/order-service.git"),
-    DATA_SYNC_SERVICE("data-sync-service","","https://git.silvrr.com/aku-ec-service/data-sync-service.git"),
-    CALCULATION_SERVICE("calculation-service","calculation-service-impl","https://git.silvrr.com/aku-ec-service/calculation-service.git"),
-    AKU_PUSH_SERVICE("aku-push-service","aku-push-service-server","https://git.silvrr.com/aku-ec-service/aku-push-service.git"),
-    TRADE_BIZ("trade-biz","trade-biz-app","https://git.silvrr.com/aku-ec-service/trade-biz.git"),
-    CONSOLE_BIZ_ORDER("console-biz-order","","https://git.silvrr.com/aku-ec-biz/console-biz-order.git"),
-    FULFILLMENT_SERVICE("fulfillment-service","fulfillment-service","https://git.silvrr.com/aku-ec-service/fulfillment-parent.git"),
-    ORDER_SETTLEMENT_SERVICE("order-settlement-service","order-settlement-server","https://git.silvrr.com/aku-ec-service/order-settlement-service.git"),
-    ORDER_SERVICE("order-service","order-service-impl","https://git.silvrr.com/aku-ec-service/order-service.git"),
+    ORDER_SERVICE_JOB("order-service-job","order-service-job","https://xxxxxxxx.com/aku-ec-service/order-service.git"),
+    DATA_SYNC_SERVICE("data-sync-service","","https://xxxxxxxx.com/aku-ec-service/data-sync-service.git"),
+    CALCULATION_SERVICE("calculation-service","calculation-service-impl","https://xxxxxxxx.com/aku-ec-service/calculation-service.git"),
+    AKU_PUSH_SERVICE("aku-push-service","aku-push-service-server","https://xxxxxxxx.com/aku-ec-service/aku-push-service.git"),
+    TRADE_BIZ("trade-biz","trade-biz-app","https://xxxxxxxx.com/aku-ec-service/trade-biz.git"),
+    CONSOLE_BIZ_ORDER("console-biz-order","","https://xxxxxxxx.com/aku-ec-biz/console-biz-order.git"),
+    FULFILLMENT_SERVICE("fulfillment-service","fulfillment-service","https://xxxxxxxx.com/aku-ec-service/fulfillment-parent.git"),
+    ORDER_SETTLEMENT_SERVICE("order-settlement-service","order-settlement-server","https://xxxxxxxx.com/aku-ec-service/order-settlement-service.git"),
+    ORDER_SERVICE("order-service","order-service-impl","https://xxxxxxxx.com/aku-ec-service/order-service.git"),
 
     ;
     /**
